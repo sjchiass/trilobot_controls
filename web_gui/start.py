6a4ac9dbdeea27ef2b319757466ab3d271250da1
Sebastien Chiasson <sjchiass@gmail.com>
2022-12-28 13:48:45 -0500

    Add a starting script

---
+++ web_gui/start.py
@@ -0,0 +1,38 @@
+import subprocess
+import atexit
+import socket
+
+print("*** Starting MJPEG server ...***")
+mjpeg_server = subprocess.Popen(
+    ["python3", "./mjpeg.py"],
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.STDOUT)
+
+print("*** Starting API server ...  ***")
+fastapi_server = subprocess.Popen(
+    ["uvicorn", "app:app", "--host", "0.0.0.0"],
+    stdout=subprocess.DEVNULL,
+    stderr=subprocess.STDOUT)
+
+# Prepare a function on exit to shutdown the servers
+def shutdown():
+    mjpeg_server.kill()
+    fastapi_server.kill()
+
+
+# Get IP address
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
+
+print("*** Access the controls here ***")
+print(f"--> http://{local_ip}:8000/")
+print("*** Press CTRL+C to shutdown ***")
+
+# Keep going until done
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    pass
+
+atexit.register(shutdown)
